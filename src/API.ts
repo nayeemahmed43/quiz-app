c811faeef1acd715442b03eb186de2b02922b848
unknown <you@example.com>
2021-08-23 23:34:43 +0600

    api endpoint set up

---
+++ src/API.ts
@@ -0,0 +1,13 @@
+export enum Difficulty {
+  Easy = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard',
+}
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endPoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endPoint)).json();
+  console.log(data);
+};
